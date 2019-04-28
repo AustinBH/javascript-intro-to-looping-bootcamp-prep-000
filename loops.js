9256e7efa22c320c8c3828727a54bdb147b61670
Austin Harlow <harlowaustin2@gmail.com>
2019-04-28 22:08:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,11 @@
-for (let i = 0; i < 25; i++) {
-  if (i = 1) {
-    return "I am 1 strange loop."
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push("I am ${i} strange loops.")
+    }
   }
-  else {
-    return "I am ${i} strange loops."
-  }
+return array;
 }
